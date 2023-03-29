236ed49907b9bb6098902812968afbfcbe3ed660
Abhijeet Sinha <abhijeetsinha.smith@gmail.com>
2023-03-29 22:52:11 +0530

    UI-Commit

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import { useState } from 'react';
 import './App.css';
 import FormInfo from './components/FormInfo';
 import axios from 'axios';
+import Home from './components/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-  const [values,setValues] = useState({
+
+  const [values, setValues] = useState({
     nitrogen: "",
     phosphorus: "",
     potassium: "",
@@ -13,80 +16,91 @@ function App() {
     ph: "",
     rainfall: "",
   });
-  const [loading,setLoading] = useState()
+  const [loading, setLoading] = useState()
 
   const inputs = [
     {
-      id:1,
-      name:"nitrogen",
-      type:"number",
-      placeholder:"Nitrogen",
-      label:"Nitrogen"
+      id: 1,
+      name: "nitrogen",
+      type: "number",
+      placeholder: "Nitrogen",
+      label: "Nitrogen"
     },
     {
-      id:2,
-      name:"phosphorus",
-      type:"number",
-      placeholder:"Phosphorus",
-      label:"Phosphorus"
+      id: 2,
+      name: "phosphorus",
+      type: "number",
+      placeholder: "Phosphorus",
+      label: "Phosphorus"
     },
     {
-      id:3,
-      name:"potassium",
-      type:"number",
-      placeholder:"Potassium",
-      label:"Potassium"
+      id: 3,
+      name: "potassium",
+      type: "number",
+      placeholder: "Potassium",
+      label: "Potassium"
     },
     {
-      id:4,
-      name:"temperature",
-      type:"number",
-      placeholder:"Temperature",
-      label:"Temperature"
+      id: 4,
+      name: "temperature",
+      type: "number",
+      placeholder: "Temperature",
+      label: "Temperature"
     },
     {
-      id:5,
-      name:"humidity",
-      type:"number",
-      placeholder:"Humidity",
-      label:"Humidity"
+      id: 5,
+      name: "humidity",
+      type: "number",
+      placeholder: "Humidity",
+      label: "Humidity"
     },
     {
-      id:6,
-      name:"ph",
-      type:"number",
-      placeholder:"Ph",
-      label:"Ph"
+      id: 6,
+      name: "ph",
+      type: "number",
+      placeholder: "Ph",
+      label: "Ph"
     },
     {
-      id:7,
-      name:"rainfall",
-      type:"number",
-      placeholder:"Rainfall",
-      label:"Rainfall"
+      id: 7,
+      name: "rainfall",
+      type: "number",
+      placeholder: "Rainfall",
+      label: "Rainfall"
     }
   ]
+  // Can be extracted sapareately
+  const FormComponet = () => {
+    return (
+      <div className='body'>
+        <form onSubmit={handleSubmit}>
+          <h1>Crop Recomendation</h1>
+          {inputs.map((input) => (
+            <FormInfo key={input.id} {...input} value={values[input.name]} onChange={onChange} />
+          ))}
+          <button>{loading ? 'Evaluating...' : 'Submit'}</button>
+        </form>
+      </div>)
+  }
   const handleSubmit = async (event) => {
 
     event.preventDefault();
     setLoading(true)
-    const { data } =  await axios.post(`https://cropforesight-backend.onrender.com/predict`, { nitrogen: Number(values.nitrogen) , phosphorus: Number(values.phosphorus) , potassium: Number(values.potassium) , temperature: Number(values.temperature) , humidity: Number(values.humidity) , ph: Number(values.ph) , rainfall: Number(values.rainfall) })
+    const { data } = await axios.post(`https://cropforesight-backend.onrender.com/predict`, { nitrogen: Number(values.nitrogen), phosphorus: Number(values.phosphorus), potassium: Number(values.potassium), temperature: Number(values.temperature), humidity: Number(values.humidity), ph: Number(values.ph), rainfall: Number(values.rainfall) })
     setLoading(false)
     alert(`You should plant ${data.result} in your field`);
   }
   const onChange = (event) => {
-    setValues({...values, [event.target.name]: event.target.value})
+    setValues({ ...values, [event.target.name]: event.target.value })
   }
   return (
-    <div className='body'>
-    <form onSubmit={handleSubmit}>
-      <h1>Crop Recomendation</h1>
-      {inputs.map((input) => (
-            <FormInfo key = {input.id} {...input} value={values[input.name]} onChange={onChange} />
-      ))}
-      <button>{loading ? 'Evaluating...' : 'Submit'}</button>
-    </form>
-</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/form' element={<FormComponet />} />
+      </Routes>
+
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import video from '../assets/nature.mp4'
+import './Home.css'
+const Home = () => {
+  const navigate = useNavigate()
+  return (
+    <div className='main'>
+      <video src={video} autoPlay loop muted />
+      <div className='overlay'></div>
+      <div className='content'>
+        <h1 className='text'>Crop Prdiction</h1>
+        <p className='para'>Predict what type of crop is suitable on your land</p>
+      </div>
+      <div className='button'>
+        <button className='button1' onClick={() => (navigate("/form"))}>PREDICT YOUR CROP</button>
+      </div>
+    </div>
+  )
+}
+
+export default Home
