51c89ebcfdf785f611252d637a3490ff54398a57
Somenath Choudhury <somenathchoudhury35@gmail.com>
2023-04-02 19:59:02 +0530

    commited

--- src/components/FormInfo.js
+++ src/components/FormInfo.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import './formInput.css'
 const FormInfo = (props) => {
-  const{label,onChange,id,...inputProp} = props
+  const { label, onChange, id, ...inputProp } = props
   return (
     <div className='formInput'>
       <label>{label}</label>
-      <input {...inputProp} onChange={onChange}/>
+      <input {...inputProp} onChange={onChange} required />
     </div>
   )
 }
