7f0449e2670714b903a29f7762d0c3271a50a7fc
Narra_Venkata_Raghu_Charan <narravenkataraghucharan03@gmail.com>
2023-05-26 17:42:37 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,32 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-// Check if the user prefers dark mode
 const prefersDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-
-// Set the initial mode based on user preference
-const initialMode = prefersDarkMode ? 'dark' : 'light';
-
-// Function to toggle the mode
-const toggleMode = () => {
-  document.documentElement.classList.toggle('dark-mode');
-};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App darkMode={prefersDarkMode} />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// Add event listener to toggle mode when the button is clicked
-const modeToggleBtn = document.getElementById('mode-toggle-btn');
-if (modeToggleBtn) {
-  modeToggleBtn.addEventListener('click', toggleMode);
-}
-
-// Set the initial mode on page load
-if (initialMode === 'dark') {
-  toggleMode();
-}
+reportWebVitals();
