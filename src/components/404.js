896bad336254a89e4d322c29a665d0bb7c9af6ce
Somenath Choudhury <somenathchoudhury35@gmail.com>
2023-06-03 12:58:19 +0530

    added navbar in 404 page

--- src/components/404.js
+++ src/components/404.js
@@ -1,11 +1,34 @@
+import { Link } from "react-router-dom";
+import logo from '../assets/earth.png';
 import "./404.css";
 
 const Err = () => {
-    return(
-        <div className="pg_not_found"> 
-        <h1 className="error">4<span><i class="fas fa-ghost"></i></span>4</h1>
-        <h2>Error: 404 page not found</h2>
-        <p>Sorry, the page you're looking for cannot be accessed</p></div>
+    return (
+        <>
+            <div className='nav'>
+                <header>
+
+                    <nav>
+                        <div class="left" style={{ color: "red" }}>
+                            <Link to="/"><img src={logo} /></Link>
+                        </div>
+                        <div class="right">
+                            <ul>
+                                <li><Link to="/">Home</Link></li>
+                                <li><Link to="/about">About</Link></li>
+                                <li><Link to="/contact" >Contact</Link></li>
+
+                            </ul>
+                        </div>
+                    </nav>
+                </header>
+            </div>
+            <div className="pg_not_found">
+                <h1 className="error">4<span><i class="fas fa-ghost"></i></span>4</h1>
+                <h2>Error: 404 page not found</h2>
+                <p>Sorry, the page you're looking for cannot be accessed</p>
+            </div>
+        </>
     )
 }
 
