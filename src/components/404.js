8822e8099f201ba25ba8d44bc18167345d9fae58
akuldeepj <akuldeepj@gmail.com>
2023-06-17 14:17:46 +0530

    responsive contact page

--- src/components/404.js
+++ src/components/404.js
@@ -25,7 +25,7 @@ const Err = () => {
                 </header>
             </div>
             <div className="pg_not_found">
-                <h1 className="error">4<span><i class="fas fa-ghost"></i></span>4</h1>
+                <h1 className="error">4<span><i class="fas fa-ghost purple"></i></span>4</h1>
                 <h2>Error: 404 page not found</h2>
                 <p>Sorry, the page you're looking for cannot be accessed</p>
             </div>
