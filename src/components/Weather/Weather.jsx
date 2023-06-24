777c78a5436144346f03b68f3d18b63567ae5255
owais <owaissalauddinkhan@gmail.com>
2023-06-24 13:32:38 +0530

    added more attributes of weather condition

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -1,47 +1,49 @@
-import React,{useState} from 'react'
-import axios from 'axios'
-import './Weather.css'
+import React, { useState } from "react";
+import axios from "axios";
+import "./Weather.css";
 
 export const Weather = () => {
-    const [city,setCity] = useState('')
-    const[weatherData,setWeatherData] = useState(null);
+  const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
 
-    const handleWeather = async () =>{
-        try {
-            const response = await axios.get(
-                `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=7b96e250f5f9d8682865d4335a802fe2`
-            );
-            setWeatherData(response.data)
-        }catch(error){
-            console.error(error)
-
-        }
+  const handleWeather = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=7b96e250f5f9d8682865d4335a802fe2`
+      );
+      setWeatherData(response.data);
+    } catch (error) {
+      console.error(error);
     }
-
-
+  };
 
   return (
     <div>
-        <h1>Weather</h1>
-        <form className="form-h">
+      <h1>Weather</h1>
+      <form className="form-h">
         <input
-        type="text"
-        id="search" placeholder="Search By Loaction"
-        value = {city}
-        onChange={(e) => setCity(e.target.value)}
+          type="text"
+          id="search"
+          placeholder="Search By Loaction"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
         />
-        </form>
-         <button onClick={handleWeather} className="getWeather">SUBMIT</button>
-         {weatherData &&(
-             <div className='Weather'>
-
-                 <p className='name'>{weatherData.name}</p>
-                 <p className='dis'>{weatherData.weather[0].description}</p>
-                 <p className='temp'>{weatherData.main.temp}</p>
-             </div>
-         )}
+      </form>
+      <button onClick={handleWeather} className="getWeather">
+        SUBMIT
+      </button>
+      {weatherData && (
+        <div className="Weather">
+          <p className="name">{weatherData.name}</p>
+          <p className="dis">{weatherData.weather.icon}+{weatherData.weather[0].description}</p>
+          <p className="temp">{weatherData.main.temp}</p>
+          <p className="humidity">{weatherData.main.humidity}</p>
+          <p className="temp_min">{weatherData.main.temp_min}</p>
+          <p className="temp_max">{weatherData.main.temp_max}</p>
+          <p className="wind_speed">{weatherData.wind.speed}</p>
+        </div>
+      )}
     </div>
-
-  )
-}
+  );
+};
 export default Weather;
