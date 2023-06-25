c152ab159e8980b43ee739e36c1ec756eecbcb2a
Sameep002 <108282267+Sameep002@users.noreply.github.com>
2023-06-25 13:25:42 +0530

    Update about.js

--- src/components/about.js
+++ src/components/about.js
@@ -15,66 +15,31 @@ import Review from "./Review";
 const About = () => {
   const navigate = useNavigate();
 
-  const handleClick = () => {
-    navigate("/contact"); // Replace '/other-page' with the desired URL of the page you want to navigate to
-  };
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1
-  };
-  return (
-    <>
-      {/* nav bar */}
-      <div className="nav">
-        <header>
-          <nav>
-            <div className="left" style={{ color: "red" }}>
-              <Link to="/">
-                <img src={logo} />
-              </Link>
-            </div>
-            <div className="right">
-              <ul>
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-                <li>
-                  <Link to="/about">About</Link>
-                </li>
-                <li>
-                  <Link to="/contact">Contact</Link>
-                </li>
-                <li>
-                  <Link to="/faq">FAQs</Link>
-                </li>
-                <li>
-                  <Link to="/Weather">Weather</Link>
-                </li>
-              </ul>
-            </div>
-          </nav>
-        </header>
-      </div>
+    const handleClick = () => {
+        navigate('/contact'); // Replace '/other-page' with the desired URL of the page you want to navigate to
+    };
+    return (
+        <>
+            {/* nav bar */}
+            <NAVBAR />
+            {/* main section */}
+            <div className="aboutMain">
+                <h1 className="about"> What Is CropForesight❔ </h1>
+                <div className="content">
+                    <div className="left">
+                        <p>CropForesight is a powerful crop recommendation website that helps farmers and agriculture enthusiasts make informed decisions about the best crop to cultivate on a given land.<br />
+                            By utilizing machine learning algorithms and various environmental parameters such as nitrogen value of soil, phosphorus value, rainfall, pH, potassium, humidity, and temperature. CropForesight predicts the optimal crop choice, maximizing productivity and yield.</p>
 
-      {/* main section */}
-      <div className="aboutMain">
-        <h1 className="about"> What Is CropForesight❔ </h1>
-        <div className="content">
-          <div className="left">
-            <p>
-              CropForesight is a powerful crop recommendation website that helps
-              farmers and agriculture enthusiasts make informed decisions about
-              the best crop to cultivate on a given land.
-              <br />
-              By utilizing machine learning algorithms and various environmental
-              parameters such as nitrogen value of soil, phosphorus value,
-              rainfall, pH, potassium, humidity, and temperature. CropForesight
-              predicts the optimal crop choice, maximizing productivity and
-              yield.
-            </p>
+                        <ul className="feature">
+                            <h3>Salient Features</h3>
+                            <li>
+                                Intelligent crop recommendation</li>
+                            <li>
+                                User-friendly interface </li>
+                            <li>Efficient ML model leveraging Gaussian Naïve Bayes algorithm.</li>
+                            <li>
+                                Scalable backend powered by FastAPI for quick data processing.</li>
+                        </ul>
 
             <ul className="feature">
               <h3>Salient Features</h3>
