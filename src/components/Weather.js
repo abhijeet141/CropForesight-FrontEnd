f7be4176a6b9aef63bb533ddaffaa654698386f3
Triboga21 <triboga.official@gmail.com>
2023-06-27 23:26:26 +0530

    Weather units, error message and typos

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -9,14 +9,17 @@ import NAVBAR from './nav';
 export const Weather = () => {
     const [city, setCity] = useState('')
     const [weatherData, setWeatherData] = useState(null);
+    const [submitPress, setSubmitPress] =useState(false)
 
     const handleWeather = async () => {
+        setSubmitPress(true)
         try {
             const response = await axios.get(
                 `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=7b96e250f5f9d8682865d4335a802fe2`
             );
             setWeatherData(response.data)
         } catch (error) {
+            setWeatherData(null)
             console.error(error)
 
         }
@@ -32,7 +35,7 @@ export const Weather = () => {
             <form className="form-h">
                 <input
                     type="text"
-                    id="search" placeholder="Search By Loaction"
+                    id="search" placeholder="Search By Location"
                     value={city}
                     onChange={(e) => setCity(e.target.value)}
                 />
@@ -43,7 +46,12 @@ export const Weather = () => {
 
                     <p className='name'>{weatherData.name}</p>
                     <p className='dis'>{weatherData.weather[0].description}</p>
-                    <p className='temp'>{weatherData.main.temp}</p>
+                    <p className='temp'>{(weatherData.main.temp-273.15).toFixed(2)}°C</p>
+                </div>
+            )}
+            {weatherData==null && submitPress==true && (
+                <div className='Weather'>
+                    <p className='name'>Please enter proper Location details.</p>
                 </div>
             )}
             </div>
