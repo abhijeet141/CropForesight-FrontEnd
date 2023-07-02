345c69d22cff39658fc3784b9422776ba23ff983
Manisha Swain <manishaswain2003@gmail.com>
2023-07-02 12:53:27 +0530

    Added crop example page issue-97

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import axios from 'axios';
-import { useState } from 'react';
-import './App.css';
-import Err from './components/404';
-import FormInfo from './components/FormInfo';
-import Home from './components/Home';
-import Weather from './components/Weather';
-import About from './components/about';
-import Contact from './components/contact';
-import FAQ from './components/faq/faq';
+import axios from "axios";
+import { useState } from "react";
+import "./App.css";
+import Err from "./components/404";
+import FormInfo from "./components/FormInfo";
+import Home from "./components/Home";
+import Weather from "./components/Weather";
+import About from "./components/about";
+import Contact from "./components/contact";
+import FAQ from "./components/faq/faq";
+import { ExampleCrop } from "./components/ExampleOfCrop/ExampleCrop";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import swal from "sweetalert";
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import swal from 'sweetalert';
-
-import GoToTop from './GoToTop/GoToTop';
-
-
+import GoToTop from "./GoToTop/GoToTop";
 
 function App() {
-
   const [values, setValues] = useState({
     nitrogen: "",
     phosphorus: "",
@@ -27,7 +24,7 @@ function App() {
     ph: "",
     rainfall: "",
   });
-  const [loading, setLoading] = useState()
+  const [loading, setLoading] = useState();
 
   const inputs = [
     {
@@ -35,75 +32,93 @@ function App() {
       name: "nitrogen",
       type: "number",
       placeholder: "Nitrogen",
-      label: "Nitrogen in parts per million (ppm)"
+      label: "Nitrogen in parts per million (ppm)",
     },
     {
       id: 2,
       name: "phosphorus",
       type: "number",
       placeholder: "Phosphorus",
-      label: "Phosphorus in parts per million (ppm) "
+      label: "Phosphorus in parts per million (ppm) ",
     },
     {
       id: 3,
       name: "potassium",
       type: "number",
       placeholder: "Potassium",
-      label: "Potassium in parts per million (ppm) "
+      label: "Potassium in parts per million (ppm) ",
     },
     {
       id: 4,
       name: "temperature",
       type: "number",
       placeholder: "Temperature",
-      label: "Temperature in Celsius (°C)"
+      label: "Temperature in Celsius (°C)",
     },
     {
       id: 5,
       name: "humidity",
       type: "number",
       placeholder: "Humidity",
-      label: "Humidity in percentage (%)"
+      label: "Humidity in percentage (%)",
     },
     {
       id: 6,
       name: "ph",
       type: "number",
       placeholder: "Ph",
-      label: "Ph (0-14)"
+      label: "Ph (0-14)",
     },
     {
       id: 7,
       name: "rainfall",
       type: "number",
       placeholder: "Rainfall",
-      label: "Rainfall in millimeters (mm)"
-    }
-  ]
+      label: "Rainfall in millimeters (mm)",
+    },
+  ];
   // Can be extracted sapareately
   const FormComponet = () => {
     return (
-      <div className='body'>
+      <div className="body">
         <form onSubmit={handleSubmit}>
-          <h1 className='title'>Crop Recomendation</h1>
+          <h1 className="title">Crop Recomendation</h1>
           {inputs.map((input) => (
-            <FormInfo key={input.id} {...input} value={values[input.name]} onChange={onChange} />
+            <FormInfo
+              key={input.id}
+              {...input}
+              value={values[input.name]}
+              onChange={onChange}
+            />
           ))}
-          <button className='btn'>{loading ? 'Evaluating...' : 'Recommend Crop'}</button>
+          <button className="btn">
+            {loading ? "Evaluating..." : "Recommend Crop"}
+          </button>
         </form>
-      </div>)
-  }
+      </div>
+    );
+  };
   const handleSubmit = async (event) => {
-
     event.preventDefault();
-    setLoading(true)
-    const { data } = await axios.post(`https://cropforesight-backend.onrender.com/predict`, { nitrogen: Number(values.nitrogen), phosphorus: Number(values.phosphorus), potassium: Number(values.potassium), temperature: Number(values.temperature), humidity: Number(values.humidity), ph: Number(values.ph), rainfall: Number(values.rainfall) })
-    setLoading(false)
+    setLoading(true);
+    const { data } = await axios.post(
+      `https://cropforesight-backend.onrender.com/predict`,
+      {
+        nitrogen: Number(values.nitrogen),
+        phosphorus: Number(values.phosphorus),
+        potassium: Number(values.potassium),
+        temperature: Number(values.temperature),
+        humidity: Number(values.humidity),
+        ph: Number(values.ph),
+        rainfall: Number(values.rainfall),
+      }
+    );
+    setLoading(false);
     swal("Success", `You should plant ${data.result} in your field`, "success");
-  }
+  };
   const onChange = (event) => {
-    setValues({ ...values, [event.target.name]: event.target.value })
-  }
+    setValues({ ...values, [event.target.name]: event.target.value });
+  };
 
   const [preLoading, setPreLoading] = useState(true);
   const spinner = document.getElementById("spinner");
@@ -114,20 +129,20 @@ function App() {
     }, 2000);
   }
 
-
   return (
     !preLoading && (
       <>
         <GoToTop />
         <BrowserRouter>
           <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/form' element={FormComponet()} />
-            <Route path='/about' element={<About />} />
-            <Route path='/contact' element={<Contact />} />
-            <Route path='/faq' element={<FAQ />} />
-            <Route path='/Weather' element={<Weather />} />
-            <Route path='/*' element={<Err />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/form" element={FormComponet()} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/faq" element={<FAQ />} />
+            <Route path="/Weather" element={<Weather />} />
+            <Route path="/*" element={<Err />} />
+            <Route path="/ExampleCrop" element={<ExampleCrop />} />
           </Routes>
         </BrowserRouter>
       </>
---
+++ src/components/ExampleOfCrop/ExampleCrop.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import "./ExampleCrop.css";
+import CropData from "./CropData";
+import { useState } from "react";
+import NAV from "../nav";
+
+export function ExampleCrop() {
+  const [search, setSearch] = useState("");
+
+  return (
+    <>
+      <NAV />
+      <h1 className="h1_E">Find your crop </h1>
+      <form className="form_E">
+        <input
+          placeholder="Input season to find your crop"
+          onChange={(e) => setSearch(e.target.value)}
+          type="text"
+          className="input_E"
+        ></input>
+      </form>
+
+      <table className="table_E">
+        <thead className="thead_E">
+          <tr>
+            <th>SL NO.</th>
+            <th>SEASON</th>
+            <th>CROP</th>
+            <th>IMAGE</th>
+          </tr>
+        </thead>
+        <tbody className="tbody_E">
+          {CropData.filter((card) => {
+            return search.toLowerCase() === ""
+              ? card
+              : card.season.toLowerCase().includes(search);
+          }).map((card, index) => (
+            <tr>
+              <td>{card.sl}</td>
+              <td>{card.season}</td>
+              <td>{card.head}</td>
+              <td>
+                <img src={card.imageUrl} alt="img" className="Avtar" />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+}
