9012ad527b7e5d675317c2dd02810d2cf407a691
Syed Imtiyaz Ali <syedimtiyazali141@gmail.com>
2023-08-06 17:11:26 +0530

    optimizing

--- src/components/contact.js
+++ src/components/contact.js
@@ -119,7 +119,7 @@ const Contact = () => {
 
       {/* contact us form */}
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-      <section>
+      <section className="sectionIO">
         <div className="containerIO">
           <div className="contactInfo">
             <div>
