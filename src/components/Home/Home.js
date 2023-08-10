2d43d2b25a5d7446e2c241df882149dbbc154e6f
Bharti Vilas Nagpure <104631882+BhartiNagpure@users.noreply.github.com>
2023-08-10 07:37:55 +0530

    remove border

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -35,7 +35,7 @@ const Typewriter = ({ sentences, delay }) => {
   return <span>{displayText}</span>;
 };
 
-const Home = ({mode,setmode}) => {
+const Home = ({ mode, setmode }) => {
   const navigate = useNavigate();
   const sentences = [
     "Predict what type of crop🌾 is suitable on your land🌏",
@@ -43,9 +43,9 @@ const Home = ({mode,setmode}) => {
   ];
 
   return (
-    <div style={{backgroundColor:mode==='light'?"white":""}}>
-      <div className="main" style={{backgroundColor:mode==='light'?"white":""}}>
-        <NAVBAR mode={mode} setmode={setmode}/>
+    <div className="card-container" style={{ backgroundColor: mode === 'light' ? "white" : "" }}>
+      <div className="main" style={{ backgroundColor: mode === 'light' ? "white" : "" }}>
+        <NAVBAR mode={mode} setmode={setmode} />
         <video src={video} autoPlay loop muted />
 
         {/* <div className="card glass">
@@ -66,11 +66,11 @@ const Home = ({mode,setmode}) => {
         </button>
       </div> */}
 
-        <div className={`card glass ${mode === 'light' ? "bg-dark1"  : "bg-light1"}`}>
-          <button className={`button-1 ${mode == 'light' ? "text-white": "text-black"}`} onClick={() => navigate("/form")}>
+        <div className={`card glass ${mode === 'light' ? "bg-dark1" : "bg-light1"}`}>
+          <button className={`button-1 ${mode == 'light' ? "text-white" : "text-black"}`} onClick={() => navigate("/form")}>
             PREDICT YOUR CROP
           </button>
-          <p className={`para  ${mode == 'light' ? "text-white": "text-black"}`}>
+          <p className={`para  ${mode == 'light' ? "text-white" : "text-black"}`}>
             <Typewriter sentences={sentences} delay={2000} />
           </p>
         </div>
